28d9eb4c861e6469b1a0d8b9b18f37c2c6a1ffdd
tungnt2910 <129597531+tungnt2910@users.noreply.github.com>
2023-04-01 20:01:05 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Datetime Now is :" + localDate);
+        }
+    }
+}
